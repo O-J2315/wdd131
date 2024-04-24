ecf410d977ad8b4c6331e9f23c22bee7b5d297cb
o-j2315 <her23050@byui.edu>
2024-04-24 13:14:55 -0600

    home page first try audit tool

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+let today = new Date();
+
+let yearElement = document.getElementById("currentyear");
+yearElement.innerText = `${today.getFullYear()}`;
+
+let lastMod = new Date(document.lastModified);
+let lastModEle = document.getElementById("lastModified");
+lastModEle.innerText = `${lastMod.getMonth()}/${lastMod.getDate()}/${lastMod.getFullYear()} ${lastMod.getHours()}:${lastMod.getMinutes()}:${lastMod.getSeconds()}`;
