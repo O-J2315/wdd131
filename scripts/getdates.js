c389c56baf64a1f907723f95d2aa677c5362f851
o-j2315 <her23050@byui.edu>
2024-05-07 13:41:43 -0600

    Week03 learning activities completed

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -6,3 +6,21 @@ yearElement.innerText = `${today.getFullYear()}`;
 let lastMod = new Date(document.lastModified);
 let lastModEle = document.getElementById("lastModified");
 lastModEle.innerText = `${lastMod.getMonth()}/${lastMod.getDate()}/${lastMod.getFullYear()} ${lastMod.getHours()}:${lastMod.getMinutes()}:${lastMod.getSeconds()}`;
+
+
+let scores = [100, 72, 83, 94, 88, 87];
+let accumulator = 0;
+
+let count = 0;
+scores.forEach(score => {
+  if (score > 87) {
+    accumulator += score;
+    count ++;
+  }
+});
+if ( count > 0 ) {
+  console.log(accumulator/count); 
+} else {
+  console.log("No scores reported.");
+}
+
